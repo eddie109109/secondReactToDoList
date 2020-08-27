ac391588d3afb33a0d70a8e1fe0e22d656ee14c6
jundic <jundi_chen@sfu.ca>
2020-08-26 23:11:23 -0700

    Updates

---
+++ precache-manifest.6c405ce88f4804751d1f2c33b0b1ffad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "638e49af06af8b1f8d45d549cc2e81f2",
+    "url": "/secondReactToDoList/index.html"
+  },
+  {
+    "revision": "f34616bb58fbce14633d",
+    "url": "/secondReactToDoList/static/js/2.a3870095.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/secondReactToDoList/static/js/2.a3870095.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ac663ec6da4ee522a18",
+    "url": "/secondReactToDoList/static/js/main.b4f0dacb.chunk.js"
+  },
+  {
+    "revision": "6dcb9c9ed45956aa7c6a",
+    "url": "/secondReactToDoList/static/js/runtime-main.b45ba269.js"
+  }
+]);
